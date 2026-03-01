f4ffbf432ce43b25a76ddaf81a716e4c2b171056
victor <victorcastro89@hotmail.com>
2026-03-01 14:28:38 -0300

    chore: clear roundcubemail from index for submodule add

--- roundcubemail/.ci/docker-images/run-in-copy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-mkdir -p /work
-rsync -a /app/ /work/
-$@
--- roundcubemail/plugins/xmultibox/SQL/postgres/20240911.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE identities
-    ADD xmultibox_enabled SMALLINT NOT NULL DEFAULT 0,
-    ADD xmultibox_data TEXT NULL;
